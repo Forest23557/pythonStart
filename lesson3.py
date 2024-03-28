36f2443b8af4cbf55edba83994c52c9468d63148
Peter Shulha <forest23557@gmail.com>
2024-03-28 20:56:52 +0200

    Lesson 3

---
+++ lesson3.py
@@ -0,0 +1,28 @@
+# Arithmetic Operators and Strings
+
+# Arithmetic Operators:
+# + - * / % 
+
+age1 = 18
+age2 = 12
+
+# print(age1 + age2)
+# print(age1 - age2)
+# print(age1 * age2)
+# print(age1 / age2)
+# print(age1 % age2)
+
+# Strings:
+
+sent1 = "today is a breathtaking day"
+# print(sent1)
+
+firstName = "Peter"
+lastName = "Parker"
+
+# print(firstName + " " + lastName)
+# print("Hi " * 10) # 10 times
+
+print(sent1[0]) # t
+print(sent1[1]) # o
+print(sent1[0:5]) # today, last index in Python is exclusive
