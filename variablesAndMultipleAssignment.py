20e84895d94000bc1d2bd94e4560c05125f526f0
Peter Shulha <forest23557@gmail.com>
2024-03-28 19:48:02 +0200

    Lesson 2

---
+++ variablesAndMultipleAssignment.py
@@ -0,0 +1,24 @@
+# Variables and multiple assignment
+
+# age = 25
+# ageString = "25"
+# name = "Josh"
+
+# print("My name is " + name + ". I am " + ageString + ".")
+
+# print(age)
+
+sarah, bob, mike = 21, 22, 25
+
+# print(sarah)
+# print(bob)
+# print(mike)
+
+josh = danzel = melissa = 17
+
+# print(josh)
+# print(danzel)
+# print(melissa)
+
+name, age = "Peter", 24
+print(name, age)
