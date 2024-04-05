c01af3116fd68c96aeff45e1d537d6be8342f37f
Peter Shulha <forest23557@gmail.com>
2024-04-05 22:17:06 +0300

    Free Code Camp - first part

---
+++ basicTermsAndFeatures.py
@@ -0,0 +1,74 @@
+# Variables (We can use characters, numbers and underscore to a label of a variable)
+
+# _some = 5
+# _some_value = 10
+# value2 = 5
+# some = 2
+# value_2 = 3
+# HEIGHT = 8
+# Height = "String"
+
+# Expressions and statements
+
+# Expressions return a value
+# 1+1
+# "smth"
+
+# Statements do some operations with a value
+# number = 1
+# print(number)
+# name = "John"; print(name)
+# Space (Indentation) and tabulations have a spacial meaning in Python
+ 
+# Data types
+
+# String
+# name = "Clark"
+# print(type(name)) # <class 'str'>
+# type() is a function to feagure out what type has a value
+# print(type(name) == str)
+# isinstance(value, type that we want to compare to)
+# print(isinstance(name, str)) # True
+
+# Integer
+# age = 2 
+# print(isinstance(age, int))
+
+# Float (Decimal number)
+# float_number = 4.56
+# print(isinstance(float_number, float))
+
+# Convert from one type to anoter
+# number = float(2)
+# print(isinstance(number, float)) # True
+
+# number = "5"
+# string_number = int(number) # Casting from integer to string
+# print(isinstance(string_number, int)) # True
+
+# Types in Python:
+# int for integers
+# str for strings
+# float for decimal numbers
+# comlex for complex numbers
+# bool for booleans
+# list for lists
+# tuple for tuples
+# range for ranges
+# dict for dictionaries
+# set for sets
+
+# Operators
+
+#Arithmetic Operators
+1 + 1 #2 Addition
+2 - 1 #1 Subtraction
+2 * 2 #4 Multiplication
+4 / 2 #2 Division
+4 % 3 #1 Remainder
+4 ** 2 #16 Exponentiation
+4 // 2 #2 Floor division rounds down 
+# In Python, we can perform floor division (also sometimes known as integer division) using the // operator. This operator will divide the first argument by the second and round the result down to the nearest whole number, making it equivalent to the math. floor() function
+1 + -1 #0
+
+# print("Champ " + "is a good boy " + str(5))
